d74aabc982bde9b5d885e09ce49038492bf0c9a3
Bhavik Tailor <bhavik.t1996@gmail.com>
2016-12-07 12:24:23 -0500

    init and linked list

---
+++ part2.c
@@ -0,0 +1,183 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct infoTuple{
+    char accountNo[5];
+    char PIN[3];
+    float funds;
+} infoTuple;
+
+struct node
+{
+    infoTuple data;
+    struct node *next;
+}*head;
+ 
+void append(infoTuple num)
+{
+    struct node *temp,*right;
+    temp= (struct node *)malloc(sizeof(struct node));
+    temp->data=num;
+    right=(struct node *)head;
+    while(right->next != NULL)
+    right=right->next;
+    right->next =temp;
+    right=temp;
+    right->next=NULL;
+}
+ 
+ 
+ 
+void add(infoTuple num )
+{
+    struct node *temp;
+    temp=(struct node *)malloc(sizeof(struct node));
+    temp->data=num;
+    if (head== NULL)
+    {
+    head=temp;
+    head->next=NULL;
+    }
+    else
+    {
+    temp->next=head;
+    head=temp;
+    }
+}
+
+/*
+void addafter(int num, int loc)
+{
+    int i;
+    struct node *temp,*left,*right;
+    right=head;
+    for(i=1;i<loc;i++)
+    {
+    left=right;
+    right=right->next;
+    }
+    temp=(struct node *)malloc(sizeof(struct node));
+    temp->data=num;
+    left->next=temp;
+    left=temp;
+    left->next=right;
+    return;
+}*/
+ 
+ 
+ 
+void insert(infoTuple num)
+{
+    int c=0;
+    struct node *temp;
+    temp=head;
+    if(temp==NULL)
+    {
+    add(num);
+    }
+    else
+    {
+    while(temp!=NULL)
+    {
+        if(temp->data<num)
+        c++;
+        temp=temp->next;
+    }
+    if(c==0)
+        add(num);
+    else if(c<count())
+        addafter(num,++c);
+    else
+        append(num);
+    }
+}
+ 
+ 
+ 
+int delete(infoTuple num)
+{
+    struct node *temp, *prev;
+    temp=head;
+    while(temp!=NULL)
+    {
+    if(temp->data==num)
+    {
+        if(temp==head)
+        {
+        head=temp->next;
+        free(temp);
+        return 1;
+        }
+        else
+        {
+        prev->next=temp->next;
+        free(temp);
+        return 1;
+        }
+    }
+    else
+    {
+        prev=temp;
+        temp= temp->next;
+    }
+    }
+    return 0;
+}
+ 
+ 
+void  display(struct node *r)
+{
+    r=head;
+    if(r==NULL)
+    {
+    return;
+    }
+    while(r!=NULL)
+    {
+    printf("%d ",r->data);
+    r=r->next;
+    }
+    printf("\n");
+}
+ 
+ 
+int count()
+{
+    struct node *n;
+    int c=0;
+    n=head;
+    while(n!=NULL)
+    {
+    n=n->next;
+    c++;
+    }
+    return c;
+}
+
+void server(){
+
+}
+
+void ATM(){
+
+}
+
+void editor(){
+
+}
+
+void init(){
+    infoTuple firstRow = {.accountNo = "00001", .PIN = "107", .funds = 3443.22 };
+    infoTuple secondRow = {.accountNo = "00011", .PIN = "323", .funds = 10089.97 };
+    infoTuple thirdRow = {.accountNo = "00117", .PIN = "259", .funds = 112.00 };
+    head = NULL;
+    insert(firstRow);
+    insert(secondRow);
+    insert(thirdRow);
+}
+
+int main (void){
+    init();
+}
