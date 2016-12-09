de30da8b7b691bc00cdcf606b04e155f86a32e5d
Taz-z <tanzim_zaman@hotmail.com>
2016-12-09 16:14:39 -0500

    lol

--- part2.c
+++ part2.c
@@ -4,6 +4,9 @@
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
 typedef struct {
     char accountNo[5];
@@ -23,99 +26,6 @@ typedef struct {
 	float funds;
 }Message;
 
-void append(infoTuple num)
-{
-    struct node *temp,*right;
-    temp= (struct node *)malloc(sizeof(struct node));
-    temp->data=num;
-    right=(struct node *)head;
-    while(right->next != NULL)
-    right=right->next;
-    right->next =temp;
-    right=temp;
-    right->next=NULL;
-}
- 
- 
- 
-void add(infoTuple num )
-{
-    struct node *temp;
-    temp=(struct node *)malloc(sizeof(struct node));
-    temp->data=num;
-    if (head== NULL)
-    {
-    head=temp;
-    head->next=NULL;
-    }
-    else
-    {
-    temp->next=head;
-    head=temp;
-    }
-}
- 
-void  display(struct node *r)
-{
-    r=head;
-    if(r==NULL)
-    {
-    return;
-    }
-    while(r!=NULL)
-    {
-    printf("%0.2f ",r->data.funds);
-    r=r->next;
-    }
-    printf("\n");
-}
- 
- 
-int count()
-{
-    struct node *n;
-    int c=0;
-    n=head;
-    while(n!=NULL)
-    {
-    n=n->next;
-    c++;
-    }
-    return c;
-}
-
-void server(){
-	Message receivedMessage;
-	infoTuple dbRow;
-
-	//probably want to make these constants
-	char* updateMessage = "UPDATE DB";
-	char* requestFunds = "FUNDS";
-	char* withdrawMsg = "WITH";
-	char* pinMsg = "PIN";
-
-	char* message = receivedMessage.message;
-
-	if (strcmp(message, updateMessage) == 0){
-		updateDatabase(receivedMessage);
-	} else if (strcmp(message, requestFunds) == 0) {
-		//just send funds from row to ATM thread
-	} else if (strcmp(message, withdrawMsg) == 0) {
-		//float money = dbRow.funds - receivedMessage.funds;
-		float temp = 343.30;
-		char update[10];
-		sprintf(update, "%.2f", temp);
-		FILE* file = fopen("DataBase.txt", "r+");
-		fseek(file, 9, SEEK_CUR);
-		fputs(update, file);
-	} else if (strcmp(message, pinMsg) == 0) {
-		if(checkForAccount(&receivedMessage, "DataBase.txt") == 1) {
-			//sendgoodmessage
-		} else {
-			//sendbadMessage
-		}
-	}
-}
 
 void sendMessage(Message message) {
 	
@@ -132,7 +42,7 @@ void updateDatabase(Message receivedMessage){
 int promptForFunds(Message* toSend) {
 	int check;
 	printf("Enter the funds for this account: ");
-	check = scanf("%d", &(toSend->funds));
+	check = scanf("%f", &(toSend->funds));
 	if(check == 1) return 1;
 	return -1;
 
@@ -141,7 +51,7 @@ int promptForFunds(Message* toSend) {
 float promptForWithdrawAmount() {
 	float withdraw;
 	printf("Enter the amount of money to withdaw: ");
-	scanf("%d", &withdraw);
+	scanf("%f", &withdraw);
 	if(withdraw > -1) return withdraw;
 	else return -1;
 }
@@ -155,7 +65,7 @@ int promptForFundsOrWithdraw() {
 	else return 0;
 }
 int promptForPIN(Message* toSend) {
-	int check, size, incorrect;
+	int check, size;
 	bool valid = true;
 	valid = true;
 	printf("\nPlease input a valid PIN (3 digits) : ");
@@ -175,7 +85,7 @@ int promptForPIN(Message* toSend) {
 
 }
 int promptForAccount(Message* toSend) {
-	int check, size, incorrect;
+	int check, size;
 	bool valid = true;
 	printf("Please input a valid account number (5 digits) : ");
 	check = scanf("%s", toSend->info.accountNo);
@@ -204,6 +114,7 @@ int checkForAccount(infoTuple* received, const char* fileName) {
 	char * vp = values;
 	char* front = values;
 	infoTuple i;
+	int rowNumber;
 	while(fgets(line, sizeof(line), file)){
 		char *p = line;
 		vp = front;
@@ -232,8 +143,10 @@ int checkForAccount(infoTuple* received, const char* fileName) {
 		
 		if(strcmp(i.accountNo, received->accountNo) == 0 && strcmp(i.PIN, received->PIN) == 0) {
 			received->funds = i.funds;
-			return 1;
- 		}	
+			return rowNumber;
+ 		}
+ 		rowNumber++;
+		
 	}
 	fclose(file);
 	return -1;
@@ -246,7 +159,6 @@ void *ATM(){
 	toSend.funds = -1;
 	int incorrect = 0;
 	int choice = 0;
-	float withdraw;
 	bool okay;
 	while(cont != 'x' && cont != 'X') {
 		while(promptForAccount(&toSend) != 1); 
@@ -271,17 +183,17 @@ void *ATM(){
 					choice = 2;
 					}
 				 if(choice == 2) {
-					while(toSend.funds = promptForWithdrawAmount() == -1){}
-					strcpy(toSend.message, "WITH");
-					sendMessage(toSend);
-					while(received == false) {}
-					strcpy(receivedMessage.message, "N");
-					if(receivedMessage.message[0] = 'N') {
-						printf("Not enough funds\n");
+					do {
 						while((toSend.funds = promptForWithdrawAmount()) == -1){}
-					} else {
-						printf("Enough funds available\n");
-					}
+						strcpy(toSend.message, "WITH");
+						sendMessage(toSend);
+						while(received == false) {}
+						strcpy(receivedMessage.message, "N");
+						if(receivedMessage.message[0] == 'N') {
+							printf("Not enough funds\n");
+						} 
+					} while(receivedMessage.message[0] == 'N');
+				 printf("Enough funds available\n");
 				}
 				
 				
@@ -301,7 +213,41 @@ void *ATM(){
 	}
 }
 
-void editor(){
+void server(){
+	Message receivedMessage;
+	infoTuple dbRow;
+	int rowNumber;
+
+	//probably want to make these constants
+	char* updateMessage = "UPDATE DB";
+	char* requestFunds = "FUNDS";
+	char* withdrawMsg = "WITH";
+	char* pinMsg = "PIN";
+
+	char* message = receivedMessage.message;
+
+	if (strcmp(message, updateMessage) == 0){
+		updateDatabase(receivedMessage);
+	} else if (strcmp(message, requestFunds) == 0) {
+		//just send funds from row to ATM thread
+	} else if (strcmp(message, withdrawMsg) == 0) {
+		//float money = dbRow.funds - receivedMessage.funds;
+		float temp = 343.30;
+		char update[10];
+		sprintf(update, "%.2f", temp);
+		FILE* file = fopen("DataBase.txt", "r+");
+		fseek(file, 9, SEEK_CUR);
+		fputs(update, file);
+	} else if (strcmp(message, pinMsg) == 0) {
+		if((rowNumber = checkForAccount(&dbRow, "DataBase.txt")) != -1) {
+			//sendMessage
+		} else {
+			//sendbadMessage
+		}
+	}
+}
+
+void *editor(){
 	Message toSend;
 	while(true) {
 		while(promptForAccount(&toSend) != 1);
@@ -312,30 +258,10 @@ void editor(){
 	}
 }
 
-void init(){
-    infoTuple firstRow = {.accountNo = "00001", .PIN = "107", .funds = 3443.22 };
-    infoTuple secondRow = {.accountNo = "00011", .PIN = "323", .funds = 10089.97 };
-    infoTuple thirdRow = {.accountNo = "00117", .PIN = "259", .funds = 112.00 };
-    head = NULL;
-    add(firstRow);
-    add(secondRow);
-    add(thirdRow);
-}
 
 int main (void){
-    //init();
-   	//pthread_t userThread;
-   	//pthread_t editorThread;
-    //ATM();
-	
-	float temp = 343.30;
-	char update[10];
-	sprintf(update, "%.2f", temp);
-	FILE* file = fopen("DataBase.txt", "r+");
-	
-	
-	fseek(file, 9, SEEK_CUR);
-	fputs(update, file);
-	
+	key_t key =  1234;
+	int x = msgget(key, IPC_CREAT | 0600);
+ 	
 
 }
