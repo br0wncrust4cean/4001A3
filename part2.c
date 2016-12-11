e474f733df3947837a17669ec4a1d64667da29fe
Taz-Z <tanzim_zaman@hotmail.com>
2016-12-10 19:31:35 -0500

    fix

--- part2.c
+++ part2.c
@@ -394,7 +394,7 @@ void *server(){
 					sprintf(currMoney, "%.2f", dbRow.funds);
 					FILE* file = fopen("DataBase.txt", "r+");
 					while(fgets(line, sizeof(line), file)){
-						if(rowNumber = iterations){
+						if(rowNumber == iterations){
 								printf("YOUR ROW/ITER: %d,%d\n", rowNumber, iterations);
 							int i;
 							int numOfDigits = checkSizeOfNum(currMoney);
