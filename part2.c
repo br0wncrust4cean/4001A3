9f8f169406675e09087d14cc491cdd78d86fa5f3
Fedora Cloud User <tanzim_zaman@hotmail.com>
2016-12-11 01:04:24 +0000

    ?

--- part2.c
+++ part2.c
@@ -393,7 +393,9 @@ void *server(){
 					FILE* file = fopen("DataBase.txt", "r+");
 					while(fgets(line, sizeof(line), file)){
 						if(rowNumber == iterations){
-							int i;
+
+printf("ROWNUMBER/IT: %d, %d", rowNumber, iterations);							
+int i;
 							int numOfDigits = checkSizeOfNum(currMoney);
 							char *zeroes = malloc(sizeof(char) * numOfDigits);
 							char *zeroesFront = zeroes;
